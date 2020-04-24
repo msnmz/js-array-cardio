883a36b91f0bf8439427eba72c06df386556e4b6
msnmz <snmez.muharrem@gmail.com>
2020-04-24 17:40:29 +0200

    Session 1 solutions and tests

--- session-1.js
+++ session-1.js
@@ -3,48 +3,63 @@
 // ex. reverseString('hello') === 'olleh'
 
 function reverseString(str) {
-  return 'Let\'s Start'
+  return str.split('').reverse().join('');
 }
-
-
 
 // CHALLENGE 2: VALIDATE A PALINDROME
 // Return true if palindrome and false if not
 // ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
 
-function isPalindrome(str) { }
-
-
+function isPalindrome(str) {
+  return str === reverseString(str);
+}
 
 // CHALLENGE 3: REVERSE AN INTEGER
 // Return an integer in reverse
 // ex. reverseInt(521) === 125
 
-function reverseInt(int) { }
-
-
+function reverseInt(num) {
+  return +reverseString('' + num);
+}
 
 // CHALLENGE 4: CAPITALIZE LETTERS
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
-function capitalizeLetters(str) { }
-
-
+function capitalizeLetters(str) {
+  return str.split(' ').map(w => w[0].toUpperCase() + w.substr(1)).join(' ');
+}
 
 // CHALLENGE 5: MAX CHARACTER
 // Return the character that is most common in a string
 // ex. maxCharacter('javascript') == 'a'
-function maxCharacter(str) { }
-
-
+function maxCharacter(str) {
+  const characters = {};
+  str.split('').forEach(ch => characters[ch] ? characters[ch]++ : characters[ch] = 1);
+  const sortedCharacters = Object.entries(characters).sort(([prevCh, prevVal], [nextCh, nextVal]) => nextVal - prevVal);
+  return sortedCharacters[0][0];
+}
 
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() { }
+function fizzBuzz() {
+  for (let i = 1; i <= 100; i++) {
+    let output = '';
+    if (i % 3 === 0) {
+      output += 'Fizz';
+    }
+    if (i % 5 === 0) {
+      output += 'Buzz';
+    }
+    console.log(output || i);
+  }
+}
 
+module.exports = {
+  reverseString,
+  isPalindrome,
+  reverseInt,
+  capitalizeLetters,
+  maxCharacter,
+  fizzBuzz
+}
 
-
-// Call Function
-const output = reverseString('hello');
-
-console.log(output);
---
+++ session-1.spec.js
@@ -0,0 +1,82 @@
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const session1 = require('./session-1');
+
+describe('Session 1', () => {
+  describe('reverseString', () => {
+    it('should reverse a string', () => {
+      expect(session1.reverseString('Hello!')).to.be.equal('!olleH');
+    });
+  });
+
+  describe('isPalindrome', () => {
+    it('should return true for palindromic string', () => {
+      expect(session1.isPalindrome('racecar')).to.be.true;
+    });
+
+    it('should return false for non-palindromic string', () => {
+      expect(session1.isPalindrome('racecar!')).to.be.false;
+    });
+  });
+
+  describe('reverseInt', () => {
+    it('should reverse an integer', () => {
+      expect(session1.reverseInt(521)).to.be.equal(125);
+    });
+  });
+
+  describe('capitalizeLetters', () => {
+    it('should change first letters to uppercase letters', () => {
+      expect(session1.capitalizeLetters('i love javascript')).to.be.equal('I Love Javascript');
+    });
+  });
+
+  describe('maxCharacter', () => {
+    it('should return the character that is most common in a string', () => {
+      expect(session1.maxCharacter('javascript')).to.be.equal('a');
+    });
+  });
+
+  describe('fizzBuzz', () => {
+    let logStub;
+
+    beforeEach(() => {
+      // "spy" on console.log with stub and preventing the default logging behavior
+      logStub = sinon.stub(console, 'log');
+    });
+
+    afterEach(() => {
+      // restore console.log
+      logStub.restore();
+    });
+
+    it('should log "Fizz" for multiplies of 3', () => {
+      session1.fizzBuzz();
+      expect(logStub.getCall(2).args).to.be.deep.equal(['Fizz']);
+      expect(logStub.getCall(5).args).to.be.deep.equal(['Fizz']);
+      expect(logStub.getCall(8).args).to.be.deep.equal(['Fizz']);
+      expect(logStub.getCall(11).args).to.be.deep.equal(['Fizz']);
+    });
+    it('should log "Buzz" for multiplies of 5', () => {
+      session1.fizzBuzz();
+      expect(logStub.getCall(4).args).to.be.deep.equal(['Buzz']);
+      expect(logStub.getCall(9).args).to.be.deep.equal(['Buzz']);
+      expect(logStub.getCall(19).args).to.be.deep.equal(['Buzz']);
+      expect(logStub.getCall(99).args).to.be.deep.equal(['Buzz']);
+    });
+    it('should log "FizzBuzz" for multiplies of 15', () => {
+      session1.fizzBuzz();
+      expect(logStub.getCall(14).calledWith('FizzBuzz')).to.be.true;
+      expect(logStub.getCall(29).calledWith('FizzBuzz')).to.be.true;
+      expect(logStub.getCall(59).calledWith('FizzBuzz')).to.be.true;
+      expect(logStub.getCall(89).calledWith('FizzBuzz')).to.be.true;
+    });
+    it('should log number if not multiply of 3 and 5', () => {
+      session1.fizzBuzz();
+      expect(logStub.getCall(0).calledWith(1)).to.be.true;
+      expect(logStub.getCall(6).calledWith(7)).to.be.true;
+      expect(logStub.getCall(93).calledWith(94)).to.be.true;
+      expect(logStub.getCall(97).calledWith(98)).to.be.true;
+    });
+  });
+});
